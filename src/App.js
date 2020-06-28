4c64cfeeabe984aec2090300ce1db1a47997ee4b
吉崎和真 <f14vn@MacBook-Pro.local>
2020-06-28 15:45:57 +0900

    Make it possible with React

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br/>
+        <p>Make it possible with React</p>
         <a
           className="App-link"
           href="https://reactjs.org"
